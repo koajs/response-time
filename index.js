c0c5b269841659fd6f1d382589b2ec6f21d2f42b
Jonathan Ong <jonathanrichardong@gmail.com>
2014-03-23 14:39:21 -0700

    round response-times

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function responseTime() {
   return function *responseTime(next){
     var start = Date.now();
     yield* next;
-    var delta = Date.now() - start;
+    var delta = Math.ceil(Date.now() - start);
     this.set('X-Response-Time', delta + 'ms');
   }
 }
