d5929672f29d9ac27b4c365ca299599f1fbabc1b
Ming Chen <Ming_Chen@symantec.com>
2018-11-23 09:06:00 -0800

    Use hrtime to support nano time and improve performance

--- Readme.md
+++ Readme.md
@@ -11,6 +11,30 @@
 $ npm install koa-response-time
 ```
 
+## Usage
+
+Basic usage:
+
+    var Koa = require('koa');
+    var responseTime = require('koa-response-time');
+    var app = new Koa();
+
+    app.use(responseTime());
+
+If you need response high resolution in nano time, set `highResolution` option to `true`:
+
+    app.use(responseTime(highResolution: true));
+
+
+Sample response header with `highResolution = false` (default):
+
+    X-Response-Time: 153ms
+
+Sample response header with `highResolution = true`:
+
+    X-Response-Time: 153.812ms
+
+
 ## Note
 
   Best to `.use()` at the _top_ before any other middleware,
--- index.js
+++ index.js
@@ -6,16 +6,29 @@ module.exports = responseTime;
 
 /**
  * Add X-Response-Time header field.
+ * @param {Dictionary} options options dictionary. { highResolution }
  *
+ *        highResolution: boolean.
+ *          `true` to return time in nanoseconds.
+ *          `false` to return time in milliseconds.
+ *          Default is `false` to keep back compatible.
  * @return {Function}
  * @api public
  */
 
-function responseTime() {
+function responseTime(options) {
+  var highResolution = options && options.highResolution;
   return function responseTime(ctx, next){
-    var start = Date.now();
+    var start = process.hrtime();
     return next().then(function () {
-      var delta = Math.ceil(Date.now() - start);
+      var delta = process.hrtime(start);
+
+      // Format to high resolution time with nano time
+      delta = delta[0] * 1000 + delta[1]/1000000;
+      if (!highResolution) {
+        // truncate to milliseconds.
+        delta = delta.toFixed(3);
+      }
       ctx.set('X-Response-Time', delta + 'ms');
     });
   }
