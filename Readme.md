b6da8494ec0db06b2124c59b2a4052ccebd81b65
Ming Chen <mingchen@users.noreply.github.com>
2018-11-26 20:49:27 -0800

    Fix issue #5: Use hrtime to support nano time and improve performance (#10)
    * Use hrtime to support nano time and improve performance
    
    * Change option name `highResolution` to `hrtime`.
    Add markdown highlight.

--- Readme.md
+++ Readme.md
@@ -11,6 +11,37 @@
 $ npm install koa-response-time
 ```
 
+## Usage
+
+  Basic usage:
+
+```js
+var Koa = require('koa');
+var responseTime = require('koa-response-time');
+var app = new Koa();
+
+app.use(responseTime());
+```
+
+  If you need response high resolution in nano time, set `hrtime` option to `true`:
+
+```js
+app.use(responseTime(hrtime: true));
+```
+
+
+  Sample response header with `hrtime = false` (default):
+
+```
+X-Response-Time: 153ms
+```
+
+  Sample response header with `hrtime = true`:
+
+```
+X-Response-Time: 153.123581ms
+```
+
 ## Note
 
   Best to `.use()` at the _top_ before any other middleware,
--- index.js
+++ index.js
@@ -6,16 +6,29 @@ module.exports = responseTime;
 
 /**
  * Add X-Response-Time header field.
+ * @param {Dictionary} options options dictionary. { hrtime }
  *
+ *        hrtime: boolean.
+ *          `true` to use time in nanoseconds.
+ *          `false` to use time in milliseconds.
+ *          Default is `false` to keep back compatible.
  * @return {Function}
  * @api public
  */
 
-function responseTime() {
+function responseTime(options) {
+  var hrtime = options && options.hrtime;
   return function responseTime(ctx, next){
-    var start = Date.now();
+    var start = process.hrtime();
     return next().then(function () {
-      var delta = Math.ceil(Date.now() - start);
+      var delta = process.hrtime(start);
+
+      // Format to high resolution time with nano time
+      delta = delta[0] * 1000 + delta[1]/1000000;
+      if (!hrtime) {
+        // truncate to milliseconds.
+        delta = Math.round(delta);
+      }
       ctx.set('X-Response-Time', delta + 'ms');
     });
   }
